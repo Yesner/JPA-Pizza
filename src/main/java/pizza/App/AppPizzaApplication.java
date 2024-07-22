adec881fd51bc375bbaeedc9d35545e91338b4d9
yesner <yesnersalgado@gmail.com>
2024-07-22 15:08:08 -0600

    Creation_entities

---
+++ src/main/java/pizza/App/AppPizzaApplication.java
@@ -0,0 +1,13 @@
+package pizza.App;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppPizzaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppPizzaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pizza/App/AppPizzaApplicationTests.java
@@ -0,0 +1,13 @@
+package pizza.App;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppPizzaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
